e7b0822bae216e79c59a6303dca11ff85673dbdb
TasniaAhmedTisa <tasniaahmed055@gmail.com>
2024-12-03 22:00:22 +0600

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import HomePage from './components/HomePage'
+
+function App() {
+
+  return (
+    <>
+    <HomePage></HomePage>
+      <h1>My App
+        
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className='text-2xl font-bold'>
+            This is my Home page
+        </div>
+    );
+};
+
+export default HomePage;
